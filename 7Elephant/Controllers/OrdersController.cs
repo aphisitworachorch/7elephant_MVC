1131b348224c64958765b38f518bc31dd9a25576
Asanan Aphisitworachorch <thaixperiasp@gmail.com>
2018-09-10 10:41:35 +0700

    Add Order / Edit Features

---
+++ 7Elephant/Controllers/OrdersController.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using _7Elephant.Models;
+
+namespace _7Elephant
+{
+    public class OrdersController : Controller
+    {
+        private PhoneEntities db = new PhoneEntities();
+
+        // GET: Orders
+        public ActionResult Index()
+        {
+            var order = db.Order.Include(o => o.AspNetUsers);
+            return View(order.ToList());
+        }
+        public ViewResult ViewOrders()
+        {
+            IEnumerable<OrdersViewModel> model = null;
+            model = (from cus in db.AspNetUsers
+                     join o in db.Order on cus.Id equals o.customer_id
+                     select new OrdersViewModel
+                     {
+                         CustomerID = cus.Id,
+                         CustomerName = cus.name,
+                         OrderGrand_Total = (decimal)o.grand_total,
+                         OrderStatus = o.order_status
+                     });
+            return View(model);
+        }
+        // GET: Orders/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Order.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        // GET: Orders/Create
+        public ActionResult Create()
+        {
+            ViewBag.customer_id = new SelectList(db.AspNetUsers, "Id", "Email");
+            return View();
+        }
+
+        // POST: Orders/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "order_id,customer_id,card_no,order_date,ship_date,order_status,remarks,grand_total")] Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Order.Add(order);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.customer_id = new SelectList(db.AspNetUsers, "Id", "Email", order.customer_id);
+            return View(order);
+        }
+
+        // GET: Orders/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Order.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.customer_id = new SelectList(db.AspNetUsers, "Id", "Email", order.customer_id);
+            return View(order);
+        }
+
+        // POST: Orders/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "order_id,customer_id,card_no,order_date,ship_date,order_status,remarks,grand_total")] Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(order).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.customer_id = new SelectList(db.AspNetUsers, "Id", "Email", order.customer_id);
+            return View(order);
+        }
+
+        // GET: Orders/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Order.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        // POST: Orders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Order order = db.Order.Find(id);
+            db.Order.Remove(order);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ 7Elephant/Models/OrdersViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _7Elephant.Models
+{
+    public class OrdersViewModel
+    {
+        public string CustomerID { get; set; }
+        public string CustomerName { get; set; }
+        public decimal? OrderGrand_Total { get; set; }
+        public string OrderStatus { get; set; }
+    }
+}
